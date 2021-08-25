953aa48cab798f9c47715549d71fa203d0eca50b
caito <caitocd@gmail.com>
2021-08-25 15:30:54 -0300

    implemented deleted comments only admin

--- src/app/components/commets-article/commets-article.component.ts
+++ src/app/components/commets-article/commets-article.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Comments } from 'src/app/models/comments';
 import { CommentsService } from 'src/app/services/comments.service';
+import { TokenService } from 'src/app/services/token.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-commets-article',
   templateUrl: './commets-article.component.html',
   styleUrls: ['./commets-article.component.css'],
-  providers: [CommentsService]
+  providers: [CommentsService, TokenService]
 })
 export class CommetsArticleComponent implements OnInit {
 
@@ -21,11 +23,18 @@ export class CommetsArticleComponent implements OnInit {
   totalPages: number;
   pgs: number[] = [];
   actualPage: number;
+  isAdmin: boolean;
 
-  constructor(private commentsService: CommentsService) { }
+  constructor(private commentsService: CommentsService,
+              private tokenService: TokenService) { }
 
   ngOnInit(): void {
-  
+
+    if (this.tokenService.getRoles().includes('ROLE_ADMIN')) {
+      this.isAdmin = true;
+    }else{
+      this.isAdmin = false;
+    }
     this.getCommentsPag(this.id, this.page);  
   }
 
@@ -58,5 +67,49 @@ export class CommetsArticleComponent implements OnInit {
       }
     );
   }
+
+  onDelete(id: number){
+    
+    Swal.fire({
+      title: `Eliminar Comentario`,
+      text: 'Los comentarios eliminados No podran Recuperarse',
+      icon: 'question',
+      showCloseButton: false,
+      showCancelButton: true,
+      confirmButtonColor: 'green',
+      confirmButtonText: `<i class="far fa-thumbs-up"></i>`,
+      confirmButtonAriaLabel: 'ok',
+      cancelButtonColor: 'red',
+      cancelButtonText: `<i class="far fa-thumbs-down"></i>`,
+      cancelButtonAriaLabel: 'no'
+    }).then((result) => {
+      if(result.isConfirmed){
+        this.commentsService.deleteComment(id).subscribe(
+          res => {
+            Swal.fire({
+              icon: 'success',
+              title: 'Mensaje Eliminado !!',
+              showConfirmButton: false,
+              showCancelButton: false,
+              timer: 3000
+            });
+            this.getCommentsPag(this.id, this.page);
+          },
+          err => {
+            Swal.fire({
+              icon: 'error',
+              title: `ERROR: ${err.error.message}`,
+              showCancelButton: false,
+              showConfirmButton: false,
+              timer: 3000
+            });
+            this.getCommentsPag(this.id, this.page);
+          }
+        );
+      }else{
+        return;
+      }
+    });
+  };
 
 }
